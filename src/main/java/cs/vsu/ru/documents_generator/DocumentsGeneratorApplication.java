af5fd6ba084cdda7deaf60c74ef184fd6824201a
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-05-10 23:50:56 +0300

    Create new jar file

--- src/main/java/cs/vsu/ru/documents_generator/DocumentsGeneratorApplication.java
+++ src/main/java/cs/vsu/ru/documents_generator/DocumentsGeneratorApplication.java
@@ -2,9 +2,8 @@ package cs.vsu.ru.documents_generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication()
 public class DocumentsGeneratorApplication {
 
     public static void main(String[] args) {
